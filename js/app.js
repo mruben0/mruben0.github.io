eba6aaede3fe86aa30a2aed4db9d95550649e63f
Ruben Martirosyan <mruben93@gmail.com>
2016-10-05 01:56:35 +0400

    navbar, some animation

--- js/app.js
+++ js/app.js
@@ -25,3 +25,15 @@ $scope.nextSlide = function () {
     };
 
 });
+ski.directive("scroll", function ($window) {
+    return function(scope, element, attrs) {
+        angular.element($window).bind("scroll", function() {
+             if (this.pageYOffset > 10) {
+                 scope.ChangeClass = true;
+               } else {
+                 scope.ChangeClass = false;
+             }
+            scope.$apply();
+        });
+    };
+});
